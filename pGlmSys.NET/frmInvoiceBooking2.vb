509bd0de61281dfdffdba48ac4570ebf026ba83b
cjc061000 <cc.caicedo@gmail.com>
2014-12-08 21:39:38 -0600

    guardando cambios antes de pull
    guardando pocos cambios al invoice booking antes del pull

--- pGlmSys.NET/frmInvoiceBooking2.vb
+++ pGlmSys.NET/frmInvoiceBooking2.vb
@@ -80,7 +80,7 @@ Friend Class frmInvoiceBooking
 		
 	End Sub
 	
-	'UPGRADE_WARNING: Event cbCustomer.SelectedIndexChanged may fire when form is initialized. Click for more: 'ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?keyword="88B12AE1-6DE0-48A0-86F1-60C0686C026A"'
+
 	Private Sub cbCustomer_SelectedIndexChanged(ByVal eventSender As System.Object, ByVal eventArgs As System.EventArgs) Handles cbCustomer.SelectedIndexChanged
         Dim cmd As SqlCommand = cn.CreateCommand()
 		On Error GoTo ErrorHandler
@@ -1236,7 +1236,7 @@ ErrorHandler:
             nClicks = 0
             'dgDetail.AutoSizeColumnsMode.AllCells = .Columns.a.AllowRowSizing = False
             dgDetail.AllowUserToResizeColumns = False
-            dgDetail.Height = VB6.PixelsToTwipsY(cbService.Height)
+            'dgDetail.Height = VB6.PixelsToTwipsY(cbService.Height)
             'dgDetail.TabAction = MSDataGridLib.TabActionConstants.dbgColumnNavigation
             dgDetail.StandardTab = False
 
